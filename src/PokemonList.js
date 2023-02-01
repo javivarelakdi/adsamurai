b3915469630379c05f7c42a4e208196324a201b4
Javier Varela <jvarela@strands.com>
2023-02-01 12:44:18 +0100

    fix state logic

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -2,39 +2,43 @@ import React, { useState, useEffect } from "react";
 import SelectedPokemon from "./SelectedPokemon";
 
 function PokemonList() {
-    const [pokemonUrl, setPokemonUrl] = useState("https://pokeapi.co/api/v2/pokemon/");
-    const [pokemonData, setPokemonData] = useState(null);
-    const [selectedPokemon, setSelectedPokemon] = useState(null)
-    
-    useEffect(() => {
-      fetch(
-          pokemonUrl
-        )
-          .then((res) => res.json())
-          .then((data) => {
-            setPokemonData(data);
-          });
+  const [pokemonUrl, setPokemonUrl] = useState("https://pokeapi.co/api/v2/pokemon/");
+  const [pokemonData, setPokemonData] = useState(null);
+  const [selectedPokemon, setSelectedPokemon] = useState(null)
+  const [offset, setOffset] = useState(0)
+  
+  useEffect(() => {
+    fetch(
+        pokemonUrl
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setPokemonData(data);
+        });
   }, [pokemonUrl]);
 
-    return pokemonData 
-    ? <div>
-        <button onClick={() => setPokemonUrl(pokemonData.previous !== null
-            ? pokemonData.previous
-            : pokemonUrl)}>previous</button>
-        <ul>
-          {pokemonData.results.map((item, index) => {
-            return <li key={index+1}>
-              <div>{item.name}</div>
-              <button onClick={() => setSelectedPokemon(index+1)}>select</button>
-            </li>;
-          })}
-        </ul>
-        <button onClick={() => setPokemonUrl(pokemonData.next !== null
-            ? pokemonData.next
-            : pokemonUrl)}>next</button>
-        {selectedPokemon !== null && <SelectedPokemon pokemonId={selectedPokemon}/>}
-      </div>
-    : <div>loading...</div>;
+  const move = (direction) => {
+    if (pokemonData[direction] !== null) {
+      setPokemonUrl(pokemonData[direction])
+      setOffset(direction === "previous" ? offset-20 : offset+20)
+    }
+  }
+
+  return pokemonData 
+  ? <div>
+      <button onClick={() => move("previous")}>previous</button>
+      <ul>
+        {pokemonData.results.map((item, index) => {
+          return <li key={item.url}>
+            <div>{item.name}</div>
+            <button onClick={() => setSelectedPokemon(offset+index+1)}>select</button>
+          </li>;
+        })}
+      </ul>
+      <button onClick={() => move("next")}>next</button>
+      {selectedPokemon !== null && <SelectedPokemon pokemonId={selectedPokemon}/>}
+    </div>
+  : <div>loading...</div>;
 }
 
 export default PokemonList;
